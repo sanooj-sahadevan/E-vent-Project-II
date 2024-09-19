4423abe6da681a9cb8708fb6dee9393956078fd8
sanooj <146158357+sanooj-sahadevan@users.noreply.github.com>
2024-09-19 09:51:23 +0530

    Remove .env file from tracking

--- Backend/middleware/fileUpload.ts
+++ Backend/middleware/fileUpload.ts
@@ -1,38 +1,38 @@
-// import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
-// import { IMulterFile } from '../utils/type';
-// import AWS from 'aws-sdk';
+import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
+import { IMulterFile } from '../utils/type';
+import AWS from 'aws-sdk';
 
-// export async function uploadToS3Bucket(p0: any[], file: IMulterFile): Promise<string> {
-//   try {
-//     console.log('s3');
+export async function uploadToS3Bucket(p0: any[], file: IMulterFile): Promise<string> {
+  try {
+    console.log('s3');
     
-//     if (!file) {
-//       throw new Error("No file uploaded");
-//     }
+    if (!file) {
+      throw new Error("No file uploaded");
+    }
     
-//     const params: any = {
-//       Bucket: process.env.AWS_BUCKET_NAME,
-//       Key: `posts/${Date.now()}_${file.originalname}`, // Unique file name
-//       Body: file.buffer,
-//       ContentType: file.mimetype,
-//     };
+    const params: any = {
+      Bucket: process.env.AWS_BUCKET_NAME,
+      Key: `posts/${Date.now()}_${file.originalname}`, // Unique file name
+      Body: file.buffer,
+      ContentType: file.mimetype,
+    };
 
-//     AWS.config.update({
-//       accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-//       secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-//       region: process.env.AWS_REGION,
-//     });
+    AWS.config.update({
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+      region: process.env.AWS_REGION,
+    });
 
-//     const s3 = new AWS.S3();
+    const s3 = new AWS.S3();
 
-//     const uploadedResult = await s3.upload(params).promise();
+    const uploadedResult = await s3.upload(params).promise();
 
-//     if (!uploadedResult.Location) {
-//       throw new Error("Error grabbing image URL from S3 Bucket");
-//     }
+    if (!uploadedResult.Location) {
+      throw new Error("Error grabbing image URL from S3 Bucket");
+    }
 
-//     return uploadedResult.Location;
-//   } catch (error: any) {
-//     throw new Error(error.message);
-//   }
-// }
+    return uploadedResult.Location;
+  } catch (error: any) {
+    throw new Error(error.message);
+  }
+}
