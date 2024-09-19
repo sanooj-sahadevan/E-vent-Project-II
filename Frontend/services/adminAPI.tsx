de2cfb2ecd4b7915efa2e3517580abd376c44914
sanooj <146158357+sanooj-sahadevan@users.noreply.github.com>
2024-09-20 01:09:51 +0530

    planning

--- Frontend/services/adminAPI.tsx
+++ Frontend/services/adminAPI.tsx
@@ -12,3 +12,10 @@ export const LoginAPI = async (reqBody: any)=>{
     
 return response.data
 }
+
+// 20 2 2024
+
+// IMP: profile vedor and user
+// get auditoriums, amd dishes in vedor profile
+// think the logic about user vendor profile
+// show the dishes and auditorium
