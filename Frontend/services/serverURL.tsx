dd4c81a84129de0f8aac7ef2090114978665d779
sanooj <146158357+sanooj-sahadevan@users.noreply.github.com>
2024-10-29 18:47:50 +0530

    port

--- Frontend/services/serverURL.tsx
+++ Frontend/services/serverURL.tsx
@@ -3,4 +3,3 @@ export const SERVER_URL = "http://localhost:5001/user";
 export const SERVER_URL_vendor = "http://localhost:5001/vendor";
 export const server_URL_admin ='http://localhost:5001/admin'
 export const server_URL_chat ='http://localhost:5001/chat'
-
