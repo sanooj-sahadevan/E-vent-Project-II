07d286cc494c1145f24f7ab76d624f417e6e0742
sanooj <146158357+sanooj-sahadevan@users.noreply.github.com>
2024-10-29 15:37:19 +0530

    next error

--- Frontend/app/api/paymentSuccess/route.tsx
+++ Frontend/app/api/paymentSuccess/route.tsx
@@ -1,29 +1,29 @@
 import paymentService from "@/utils/apiCalls/paymentService";
 import PayUApiCalls from "@/utils/apiCalls/PayUApiCalls";
-import { NextApiResponse } from "next";
 import { redirect } from "next/navigation";
 
-export async function POST(req: any, res: NextApiResponse) {
-  console.log('api payment sucesss');
+export async function POST(req: Request) {
+  console.log("API payment success");
+  
   const contentType = req.headers.get("content-type") || "";
   console.log({ contentType });
 
   const formData = await req.formData();
 
   const data: { [key: string]: any } = {};
-  formData.forEach((value: any, key: string) => {
+  formData.forEach((value, key) => {
     data[key] = value;
   });
-  console.log(data, 'this and all things')
+  console.log(data, "this and all things");
 
-  let PayUOrderId
   try {
-    PayUOrderId = await PayUApiCalls.saveData(data);
-    console.log(PayUOrderId, 'pauorderId');
+    const PayUOrderId = await PayUApiCalls.saveData(data);
+    console.log(PayUOrderId, "PayUOrderId");
 
-  } catch (error: any) {
-    console.log(error.message);
+    
+  } catch (error) {
+    console.error("Error:", error);
   }
   
-  redirect(`/bookingSucess`);
+  return redirect("/bookingSuccess");
 }
