26b9ef59fd71f145a9898f18b8c457900aaeffa2
sanooj <146158357+sanooj-sahadevan@users.noreply.github.com>
2024-10-31 23:40:12 +0530

    host

--- Frontend/services/serverURL.tsx
+++ Frontend/services/serverURL.tsx
@@ -10,7 +10,11 @@
 // export const server_URL_admin = "https://www.eventopia.shop/admin";
 // export const server_URL_chat = "https://www.eventopia.shop/chat";
 
-export const SERVER_URL = "https://eventopia.shop/user";
-export const SERVER_URL_vendor = "https://eventopia.shop/vendor";
-export const server_URL_admin = "https://eventopia.shop/admin";
-export const server_URL_chat = "https://eventopia.shop/chat";
+// export const SERVER_URL = "https://eventopia.shop/user";
+// export const SERVER_URL_vendor = "https://eventopia.shop/vendor";
+// export const server_URL_admin = "https://eventopia.shop/admin";
+// export const server_URL_chat = "https://eventopia.shop/chat";
+export const SERVER_URL = "https://eventopia.shop/api/user";
+export const SERVER_URL_vendor = "https://eventopia.shop/api/vendor";
+export const server_URL_admin = "https://eventopia.shop/api/admin";
+export const server_URL_chat = "https://eventopia.shop/api/chat";
