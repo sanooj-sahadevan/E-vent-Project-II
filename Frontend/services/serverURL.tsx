14380235c07c3d4391d594ea28ae9aa3ae23a83c
sanooj <146158357+sanooj-sahadevan@users.noreply.github.com>
2024-10-31 21:19:13 +0530

    hosting vercel

--- Frontend/services/serverURL.tsx
+++ Frontend/services/serverURL.tsx
@@ -1,5 +1,7 @@
 
-export const SERVER_URL = "http://localhost:5000/user";
+// export const SERVER_URL = "http://localhost:5000/user";
 export const SERVER_URL_vendor = "http://localhost:5000/vendor";
 export const server_URL_admin ='http://localhost:5000/admin'
 export const server_URL_chat ='http://localhost:5000/chat'
+// https://www.eventopia.shop/?__vercel_draft=1
+export const SERVER_URL = "https://www.eventopia.shop/user";
