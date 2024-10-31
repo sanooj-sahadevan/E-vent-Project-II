d07e9a7d12e9e491948cedd438b643931c6fdc0e
sanooj <146158357+sanooj-sahadevan@users.noreply.github.com>
2024-11-01 00:54:04 +0530

    host

--- Frontend/services/serverURL.tsx
+++ Frontend/services/serverURL.tsx
@@ -1,8 +1,8 @@
 
-// export const SERVER_URL = "http://localhost:5000/user";
-// export const SERVER_URL_vendor = "http://localhost:5000/vendor";
-// export const server_URL_admin ='http://localhost:5000/admin'
-// export const server_URL_chat ='http://localhost:5000/chat'
+export const SERVER_URL = "http://localhost:5000/user";
+export const SERVER_URL_vendor = "http://localhost:5000/vendor";
+export const server_URL_admin ='http://localhost:5000/admin'
+export const server_URL_chat ='http://localhost:5000/chat'
 // // https://www.eventopia.shop/?__vercel_draft=1
 
 // export const SERVER_URL = "https://www.eventopia.shop/user";
@@ -14,7 +14,7 @@
 // export const SERVER_URL_vendor = "https://eventopia.shop/vendor";
 // export const server_URL_admin = "https://eventopia.shop/admin";
 // export const server_URL_chat = "https://eventopia.shop/chat";
-export const SERVER_URL = "https://eventopia.shop/api/user";
-export const SERVER_URL_vendor = "https://eventopia.shop/api/vendor";
-export const server_URL_admin = "https://eventopia.shop/api/admin";
-export const server_URL_chat = "https://eventopia.shop/api/chat";
+// export const SERVER_URL = "https://eventopia.shop/api/user";
+// export const SERVER_URL_vendor = "https://eventopia.shop/api/vendor";
+// export const server_URL_admin = "https://eventopia.shop/api/admin";
+// export const server_URL_chat = "https://eventopia.shop/api/chat";
