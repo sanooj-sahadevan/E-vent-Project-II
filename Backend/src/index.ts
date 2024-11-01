dce3c2f5b005c5e2807a2e5cfa95a07a38865d8c
sanooj <146158357+sanooj-sahadevan@users.noreply.github.com>
2024-11-01 18:11:07 +0530

    render

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -19,7 +19,7 @@ import logger from "./utils/logger";
 dotenv.config();
 
 const app = express();
-const PORT = 5000;
+const PORT = 8080;
 const morganFormat = ":method :url :status :response-time ms";
 
 
