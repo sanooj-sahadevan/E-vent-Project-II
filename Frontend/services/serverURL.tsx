b085d355fd9d8d0e46d1f987ed65e56d5bb1e3ad
sanooj <146158357+sanooj-sahadevan@users.noreply.github.com>
2024-11-02 18:05:13 +0530

    render

--- Frontend/services/serverURL.tsx
+++ Frontend/services/serverURL.tsx
@@ -10,7 +10,9 @@
 // export const server_URL_admin = "https://www.eventopia.shop/admin";
 // export const server_URL_chat = "https://www.eventopia.shop/chat";
 
-export const SERVER_URL = "https://eventopia.shop/user";
+export const SERVER_URL = "https://travelie.life/v1/api/users";
+// export const SERVER_URL = "https://eventopia.shop/user";
+
 export const SERVER_URL_vendor = "https://eventopia.shop/vendor";
 export const server_URL_admin = "https://eventopia.shop/admin";
 export const server_URL_chat = "https://eventopia.shop/chat";
