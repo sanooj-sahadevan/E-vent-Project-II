b62901c571985f49759d51591a3629a368464b59
sanooj <146158357+sanooj-sahadevan@users.noreply.github.com>
2024-11-04 00:10:07 +0530

    *

--- Frontend/services/serverURL.tsx
+++ Frontend/services/serverURL.tsx
@@ -14,15 +14,16 @@
 // export const SERVER_URL = "https://eventopia.shop/user";
 // export const SERVER_URL = "https://eventopia.shop/v1/api/users";
 // export const SERVER_URL = "https://eventopia.shop/v1/api/users";
-export const SERVER_URL = "https://api.eventopia.shop/v1/api/users";
 
-export const SERVER_URL_vendor = "https://eventopia.shop/v1/api/vendor";
-export const server_URL_admin = "https://eventopia.shop/admin";
-export const server_URL_chat = "https://eventopia.shop/chat";
 
-// export const SERVER_URL = "https://e-vent-project-ii.onrender.com/user";
-// export const SERVER_URL_vendor = "https://e-vent-project-ii.onrender.com/vendor";
-// export const server_URL_admin = "https://e-vent-project-ii.onrender.com/admin";
-// export const server_URL_chat = "https://e-vent-project-ii.onrender.com/chat";
+// export const SERVER_URL = "https://api.eventopia.shop/v1/api/users";
+// export const SERVER_URL_vendor = "https://eventopia.shop/v1/api/vendor";
+// export const server_URL_admin = "https://eventopia.shop/admin";
+// export const server_URL_chat = "https://eventopia.shop/chat";
+
+export const SERVER_URL = "https://e-vent-project-ii.onrender.com/user";
+export const SERVER_URL_vendor = "https://e-vent-project-ii.onrender.com/vendor";
+export const server_URL_admin = "https://e-vent-project-ii.onrender.com/admin";
+export const server_URL_chat = "https://e-vent-project-ii.onrender.com/chat";
 
 
