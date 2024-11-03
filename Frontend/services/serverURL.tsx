9b280ce97bbdb86e71f7b88a5adc83e861ad70ff
sanooj <146158357+sanooj-sahadevan@users.noreply.github.com>
2024-11-03 21:52:35 +0530

    *

--- Frontend/services/serverURL.tsx
+++ Frontend/services/serverURL.tsx
@@ -13,9 +13,8 @@
 // export const SERVER_URL = "https://e-vent-project-ii.vercel.app/v1/api/users";
 // export const SERVER_URL = "https://eventopia.shop/user";
 // export const SERVER_URL = "https://eventopia.shop/v1/api/users";
-export const SERVER_URL = "https://eventopia.shop/v1/api/users";
-
-export const SERVER_URL_vendor = "https://eventopia.shop/v1/api/vendor";
+export const SERVER_URL = "https://eventopia.shop/v1/api/user";
+export const SERVER_URL_vendor = "https://eventopia.shop/vendor";
 export const server_URL_admin = "https://eventopia.shop/admin";
 export const server_URL_chat = "https://eventopia.shop/chat";
 
