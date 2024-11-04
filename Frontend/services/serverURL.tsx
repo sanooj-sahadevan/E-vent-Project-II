84e31572375ae39bd6a2cb95487fa36d46d0f816
sanooj <146158357+sanooj-sahadevan@users.noreply.github.com>
2024-11-04 17:55:34 +0530

    *

--- Frontend/services/serverURL.tsx
+++ Frontend/services/serverURL.tsx
@@ -15,8 +15,8 @@
 // export const SERVER_URL = "https://eventopia.shop/v1/api/users";
 // export const SERVER_URL = "https://eventopia.shop/v1/api/users";
 
-
-export const SERVER_URL = "http://13.61.23.144:8080/v1/api/user";
+//http://api.eventopia.shop/v1/api/user/vendors
+export const SERVER_URL = "http://api.eventopia.shop/v1/api/user";
 //                         http://13.61.23.144:8080/v1/api/user/vendors
 export const SERVER_URL_vendor = "https://eventopia.shop/v1/api/vendor";
 export const server_URL_admin = "https://eventopia.shop/admin";
