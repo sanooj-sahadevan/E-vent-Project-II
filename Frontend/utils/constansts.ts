16b58144cb1e67a11162f41fdba833d8cc06575d
sanooj <146158357+sanooj-sahadevan@users.noreply.github.com>
2024-11-12 10:35:37 +0530

    11-12

--- Frontend/utils/constansts.ts
+++ Frontend/utils/constansts.ts
@@ -39,9 +39,9 @@ export const PayU = {
 
 console.log('hjcbefhvbhufbvhubfhuvbrfhvbhurfbvhrbvhuvb');
 
-export const FRONTEND_DOMAIN = "http://localhost:3000";
+export const FRONTEND_DOMAIN = "https://www.eventopia.shop";
 
-const DOMAIN = "http://localhost:3000";
+const DOMAIN = "https://www.eventopia.shop";
 
 const USER_SERVICE_DOMAIN = DOMAIN;
 const POSTS_SERVICE_DOMAIN = DOMAIN;
@@ -56,9 +56,25 @@ export const MESSAGE_SERVICE_URL = `${MESSAGE_SERVICE_DOMAIN}/api/message-servic
 export const NOTIFICATION_SERVICE_URL = `${NOTIFICATION_SERVICE_DOMAIN}/api/notification-service`;
 export const ADS_SERVICE_URL = `${PAYMENT_SERVICE_DOMAIN}/api/ads-service`;
 
-export const SOCKET_URI = "http://localhost:3000";
+export const SOCKET_URI = "https://www.eventopia.shop";
 
 
 // export const JWT_SECRET =process.env.JWT_SECRET
 
 export const JWT_SECRET =   "dfnidhfhudhjvbjdbvudhvuhdfhvfheu"
+
+
+// export const PayU = {
+//   merchantKey: "PMd9OW",
+// };
+
+// //  /* PROD: =
+// export const FRONTEND_DOMAIN = "https://travelie.life";
+
+// const DOMAIN = "https://travelie.life";
+
+// export const PAYMENT_SERVICE_DOMAIN = DOMAIN;
+
+// export const SOCKET_URI = "https://travelie.life";
+
+// export const JWT_SECRET = process.env.JWT_SECRET;
