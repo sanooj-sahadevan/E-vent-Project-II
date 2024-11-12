7a1219863293366fb308982f5c62b95508a374b9
sanooj <146158357+sanooj-sahadevan@users.noreply.github.com>
2024-11-12 10:25:35 +0530

    11-12

--- Frontend/app/api/paymentSuccess/route.tsx
+++ Frontend/app/api/paymentSuccess/route.tsx
@@ -1,29 +1,59 @@
+// import paymentService from "@/utils/apiCalls/paymentService";
+// import PayUApiCalls from "@/utils/apiCalls/PayUApiCalls";
+// import { NextApiResponse } from "next";
+// import { redirect } from "next/navigation";
+
+// export async function POST(req: any, res: NextApiResponse) {
+//   console.log('api payment sucesss');
+//   const contentType = req.headers.get("content-type") || "";
+//   console.log({ contentType });
+
+//   const formData = await req.formData();
+
+//   const data: { [key: string]: any } = {};
+//   formData.forEach((value: any, key: string) => {
+//     data[key] = value;
+//   });
+//   console.log(data, 'this and all things')
+
+//   let PayUOrderId
+//   try {
+//     PayUOrderId = await PayUApiCalls.saveData(data);
+//     console.log(PayUOrderId, 'pauorderId');
+
+//   } catch (error: any) {
+//     console.log(error.message);
+//   }
+  
+//   redirect(`/bookingSucess`);
+// }
+
+
+
 import paymentService from "@/utils/apiCalls/paymentService";
 import PayUApiCalls from "@/utils/apiCalls/PayUApiCalls";
-import { NextApiResponse } from "next";
-import { redirect } from "next/navigation";
+import { NextResponse } from "next/server"; // Import NextResponse for API responses
 
-export async function POST(req: any, res: NextApiResponse) {
-  console.log('api payment sucesss');
+export async function POST(req: Request) {
+  console.log('api payment success');
+
   const contentType = req.headers.get("content-type") || "";
   console.log({ contentType });
 
   const formData = await req.formData();
-
   const data: { [key: string]: any } = {};
-  formData.forEach((value: any, key: string) => {
+  formData.forEach((value, key) => {
     data[key] = value;
   });
-  console.log(data, 'this and all things')
+  console.log(data, 'this and all things');
 
-  let PayUOrderId
+  let PayUOrderId;
   try {
     PayUOrderId = await PayUApiCalls.saveData(data);
-    console.log(PayUOrderId, 'pauorderId');
-
+    console.log(PayUOrderId, 'PayUOrderId');
   } catch (error: any) {
     console.log(error.message);
   }
-  
-  redirect(`/bookingSucess`);
+
+  return NextResponse.redirect('/bookingSuccess'); // Use NextResponse for redirection
 }
