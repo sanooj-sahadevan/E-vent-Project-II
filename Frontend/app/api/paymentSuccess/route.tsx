8dc7d3a04f5d888cbd784ea98792c2bb40109437
sanooj <146158357+sanooj-sahadevan@users.noreply.github.com>
2024-11-15 09:20:10 +0530

    UI

--- Frontend/app/api/paymentSuccess/route.tsx
+++ Frontend/app/api/paymentSuccess/route.tsx
@@ -7,47 +7,47 @@ import { redirect } from "next/navigation";
 import { NextResponse } from "next/server";
 
 
-// export async function POST(req: any) {
-//   const contentType = req.headers.get("content-type") || "";
-//   console.log({ contentType });
-// console.log('api payment sucesss');
-
-//   const formData = await req.formData();
-
-//   const data: { [key: string]: any } = {};
-//   formData.forEach((value: any, key: string) => {
-//     data[key] = value;
-//   });
-// console.log(data, 'this and all things')
-// let PayUOrderId
-//   try {
-//      PayUOrderId = await PayUApiCalls.saveData(data);
-//     // await paymentService.addTransaction(PayUOrderId, data.email, "success");
-//   } catch (error: any) {
-//     console.log(error);
-//   }
-//  redirect(`/bookingSucess`);
-
-// }
-
-
-export async function POST(req: any, res: NextApiResponse) {
+export async function POST(req: any) {
   const contentType = req.headers.get("content-type") || "";
   console.log({ contentType });
-  console.log('api payment sucesss');
+console.log('api payment sucesss');
+
   const formData = await req.formData();
 
   const data: { [key: string]: any } = {};
   formData.forEach((value: any, key: string) => {
     data[key] = value;
   });
-  console.log(data)
-  let PayUOrderId
+console.log(data, 'this and all things')
+let PayUOrderId
   try {
      PayUOrderId = await PayUApiCalls.saveData(data);
     // await paymentService.addTransaction(PayUOrderId, data.email, "success");
   } catch (error: any) {
     console.log(error);
   }
-  redirect(`/bookingSucess`);
+ redirect(`/bookingSucess`);
+
 }
+
+
+// export async function POST(req: any, res: NextApiResponse) {
+//   const contentType = req.headers.get("content-type") || "";
+//   console.log({ contentType });
+//   console.log('api payment sucesss');
+//   const formData = await req.formData();
+
+//   const data: { [key: string]: any } = {};
+//   formData.forEach((value: any, key: string) => {
+//     data[key] = value;
+//   });
+//   console.log(data)
+//   let PayUOrderId
+//   try {
+//      PayUOrderId = await PayUApiCalls.saveData(data);
+//     // await paymentService.addTransaction(PayUOrderId, data.email, "success");
+//   } catch (error: any) {
+//     console.log(error);
+//   }
+//   redirect(`/bookingSucess`);
+// }
