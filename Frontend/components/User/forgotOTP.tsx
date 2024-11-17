3d1d819fc98e9c32c304b64846a37861bf3554b1
sanooj <146158357+sanooj-sahadevan@users.noreply.github.com>
2024-11-17 21:49:55 +0530

    bug solve user profile edit

--- Frontend/components/User/forgotOTP.tsx
+++ Frontend/components/User/forgotOTP.tsx
@@ -105,7 +105,7 @@ const OTPPage: React.FC = () => {
             </div>
             <button
               type="submit"
-              className="inline-flex items-center justify-center w-full px-4 py-2 text-base font-medium text-white bg-pink-600 border border-transparent rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+              className="inline-flex items-center justify-center w-full px-4 py-2 text-base font-medium text-white bg-pink-600 border border-transparent rounded-md hover:bg-pink-700 focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-offset-2"
             >
               Verify OTP
             </button>
